9095ea449e14b76b058fe03e01ec581d233da10c
Karam Obida <karam_obida@yahoo.com>
2022-04-07 12:24:51 +0300

    Firebase production environment fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '...',
-    authDomain: 'project.firebaseapp.com',
-    databaseURL: 'https://project.firebaseio.com',
-    projectId: 'project',
-    storageBucket: 'project.appspot.com',
-    messagingSenderId: '...',
+    apiKey: "AIzaSyAhRmkIP3wlit8NCVjOO3g8iZWDoZO5te4",
+    authDomain: "mvmt-manager.firebaseapp.com",
+    databaseURL: "https://mvmt-manager-default-rtdb.firebaseio.com",
+    projectId: "mvmt-manager",
+    storageBucket: "mvmt-manager.appspot.com",
+    messagingSenderId: "706867142288",
+    appId: "1:706867142288:web:c38ddeffeeeab523940215",
+    measurementId: "G-JEZRCZQJCW"
   },
 };
